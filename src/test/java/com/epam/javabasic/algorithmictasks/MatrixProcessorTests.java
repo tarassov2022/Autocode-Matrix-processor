5164431ad8096da178fd4bb6381dc5da5baf98cd
Anton_Chyrko <Anton_Chyrko@epam.com>
2021-03-12 12:26:55 +0300

    update naming

--- src/test/java/com/epam/javabasic/algorithmictasks/MatrixProcessorTests.java
+++ src/test/java/com/epam/javabasic/algorithmictasks/MatrixProcessorTests.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 class MatrixProcessorTests extends AbstractTest {
 
     @Test
-    void testInverseMatrixTwoOnTwoFractionalValues() {
+    void shouldInverseMatrixTwoOnTwoFractionalValues() {
         double[][] inputMatrix = {{1.1, 0.3}, {-2.03, 3.67}};
 
         double[][] expectedMatrix = {{0.790, -0.065}, {0.437, 0.237}};
@@ -20,7 +20,7 @@ class MatrixProcessorTests extends AbstractTest {
     }
 
     @Test
-    void testMatrixDeterminantTwoOnTwoFractionalValues() {
+    void shouldMatrixDeterminantTwoOnTwoFractionalValues() {
         double[][] inputMatrix = {{1.1, 0.3}, {-2.03, 3.67}};
 
         double expectedDeterminant = 4.646;
@@ -32,7 +32,7 @@ class MatrixProcessorTests extends AbstractTest {
     }
 
     @Test
-    void testMultiplyTwoSquareMatricesTwoOnTwo() {
+    void shouldMultiplyTwoSquareMatricesTwoOnTwo() {
         double[][] firstMatrix = {{1, 0}, {-2, 3}};
         double[][] secondMatrix = {{-3, 15}, {5, -7}};
 
@@ -45,7 +45,7 @@ class MatrixProcessorTests extends AbstractTest {
     }
 
     @Test
-    void testMatrixOneRowTwoColumns() {
+    void shouldMatrixOneRowTwoColumns() {
         double[][] inputMatrix = {{-3, 15}};
 
         double[][] expectedMatrix = {{-3}, {15}};
@@ -57,7 +57,7 @@ class MatrixProcessorTests extends AbstractTest {
     }
 
     @Test
-    void testTurnMatrixOneRowTwoColumns() {
+    void shouldTurnMatrixOneRowTwoColumns() {
         double[][] inputMatrix = {{-3, 15}};
 
         double[][] expectedMatrix = {{-3}, {15}};
